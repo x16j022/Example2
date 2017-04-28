8e59aa49673c15c4fbd4437e31841b73f0938409
WIN_NT\x16j022 <x16j022@chiba-fjb.ac.jp>
2017-04-28 11:54:19 +0900

    四則演算

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,41 @@ namespace Example2
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcAdd_Click(object sender, EventArgs e)
+        {
+            lblAnsadd.Text = String.Format("{0}", 1 + 2);
+        }
+
+        private void btnCalcSub_Click(object sender, EventArgs e)
+        {
+            lblAnsSub.Text = String.Format("{0}", 5 - 3);
+        }
+
+        private void btnCalcMul_Click(object sender, EventArgs e)
+        {
+            lblAnsMul.Text = String.Format("{0}", 2 * 3);
+        }
+
+        private void btnCalcDiv_Click(object sender, EventArgs e)
+        {
+            lblAnsDiv.Text = String.Format("{0}", 8 / 2);
+
+        }
     }
 }
